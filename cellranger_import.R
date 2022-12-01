19225d4acb22a584c4f5d419704c3d1f91c795dc
MJBrooks <mbrooks313@gmail.com>
2022-12-01 17:02:18 -0500

    changed kallisto_import.R for using 10x h5 files and tested code

--- cellranger_import.R
+++ cellranger_import.R
@@ -1,18 +1,18 @@
 ##########################
-# This function imports kallisto scRNA-seq files into a matrix, runs doublet finder and exports Seurat list of samples
+# This function imports CellRanger scRNA-seq h5 files into a Seurat object, runs decontX and doublet finder and exports Seurat list of samples.
 
 
 
-cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=200, max.mt=20, dX=TRUE, dF=TRUE, dub_exp=0.075, dub_counts="decontXcounts", helper_dir="../src/data", out_dir="../data/processed", gtf){
+cr_import <- function(files, expt_nm, min.cells=5, min.features=200, max.mt=20, dX=TRUE, dF=TRUE, dub_exp=0.075, dub_counts="decontXcounts", helper_dir="../src/data", out_dir="../data/processed"){
+  
   
   
   ##########################
-  # This was written in Nov 22nd, 2022 by MJB
-  # USAGE: cr_import(run_info_files, expt_nm, bus_filt = TRUE, min.cell = 5, min.features = 200, max.mt = 20, helper_dir = "../src/data", out_dir = "../data/processed")
+  # This was written in Dec 1st, 2022 by MJB
+  # USAGE: cr_import(run_info_files, expt_nm, min.cell = 5, min.features = 200, max.mt = 20, helper_dir = "../src/data", out_dir = "../data/processed")
   #
   # run_info_files <- named vector of paths to the sample kallisto run_info.json
   # expt_nm <- name of the experiment, will be name of output directory of files
-  # bus_filt <- use bus filtered count, default = TRUE
   # min.cells <- minumum number of cells for gene to be kept, default = 5 
   # min.features <- minimum genes per cell to be kept, default = 200
   # max.mt <- maximum mitochondrial content per cell
@@ -22,7 +22,6 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
   # dub_counts <- count assay to use in doubletFinder, one of c("RNA", "decontXcounts")
   # helper_dir <- directory of helper scripts (tr2g.R, read_count_output.R)
   # out_dir <- directory of output directories (tr2g.R, read_count_output.R)
-  # gtf <- location of the gtf file for transcript to gene table creation
   #
   # Value
   # seu_list - Seurat object list with doublet cells filtered out
@@ -30,13 +29,10 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
   ##########################
   
   
-  # require(DropletUtils)
   require(tidyverse)
   require(Seurat)
   require(DoubletFinder)
   
-  source(file.path(helper_dir, "get_tr2g.R"))
-  source(file.path(helper_dir, "read_count_output.R"))
   source(file.path(helper_dir, "doublet_Finder.R"))
   source(file.path(helper_dir, "decontX.R"))
   
@@ -50,9 +46,6 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
   
   dir.create(out_qc, recursive = T)
   
-  # Make tr2g data.frame
-  print("Getting transcript to gene information...")
-  tr2g <- get_tr2g(gtf)
   
   # Loop for each sample in run_info_list
   for (i in 1:length(files)){
@@ -60,24 +53,13 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
     samp <- files[i]
     samp_nm <- names(files)[i]
     print(paste("Importing matrix file: ", samp_nm))
-    
-    # Import data using bustools filtered data or not
-    ifelse(bus_filt, bus <- "counts_filtered", bus <- "counts_unfiltered")
-    
-    dir_filt <- gsub("/run_info.json", "", samp)
-    dir_filt <- paste(dir_filt, bus, sep = "/")
-    res_mat_filt <- read_count_output(dir_filt[1])
-    
-    # Prep Seurat object
-    # Subset matrix for only gene IDs in non-redundant annotation
-    res_mat_mod <- res_mat_filt[rownames(res_mat_filt) %in% tr2g$gene,]
-    
-    # Change gene IDs to gene symnbols
-    rownames(res_mat_mod) <- tr2g$gene_name[match(rownames(res_mat_mod), tr2g$gene)]
+
+    # Import 10x h5 object
+    cr_h5 <- Read10X_h5(samp, use.names = TRUE, unique.features = TRUE)
     
     # Create Seurat object
     print("Creating Seurat object...")
-    seu_tmp <- CreateSeuratObject(res_mat_mod, min.cells = min.cells, min.features = min.features, project = samp_nm)
+    seu_tmp <- CreateSeuratObject(cr_h5, min.cells = min.cells, min.features = min.features, project = samp_nm)
     Idents(seu_tmp) <- samp_nm
     
     filt_min <- dim(seu_tmp)[2]
@@ -89,6 +71,7 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
     # Subset for cells with less than 20% MT
     seu_tmp <- subset(seu_tmp, subset = percent.mt < max.mt)
     mt_cells <- dim(seu_tmp)[2]
+
 
     # Run decontX to get rid of ambiant RNA contamination
     if (dX){
@@ -124,9 +107,8 @@ cr_import <- function(files, expt_nm, bus_filt=TRUE, min.cells=5, min.features=2
     }
     
     
-    
     # Get cell numbers for data.frame
-    og_cells <- dim(res_mat_filt)[2]
+    og_cells <- dim(cr_h5)[2]
     filt_cells <- dim(seu_tmp)[2]
     
     tmp_df <- tibble(c("orig_cells", 
